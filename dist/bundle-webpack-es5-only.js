67393c5ceebf5b56c83d23037748166222489f92
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-11 01:54:34 -0400

    add more dist files

---
+++ dist/bundle-webpack-es5-only.js
@@ -0,0 +1,231 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var fun1 = __webpack_require__(1);
+	var fun2 = __webpack_require__(2);
+	var fun3 = __webpack_require__(3);
+	var fun4 = __webpack_require__(4);
+	var fun5 = __webpack_require__(5);
+	var fun6 = __webpack_require__(6);
+	var fun7 = __webpack_require__(7);
+	var fun8 = __webpack_require__(8);
+	var fun9 = __webpack_require__(9);
+	var fun10 = __webpack_require__(10);
+	var fun11 = __webpack_require__(11);
+	var fun12 = __webpack_require__(12);
+	var fun13 = __webpack_require__(13);
+	var fun14 = __webpack_require__(14);
+	var fun15 = __webpack_require__(15);
+	var fun16 = __webpack_require__(16);
+	var fun17 = __webpack_require__(17);
+	var fun18 = __webpack_require__(18);
+	var fun19 = __webpack_require__(19);
+	var fun20 = __webpack_require__(20);
+
+	fun1();
+	fun2();
+	fun3();
+	fun4();
+	fun5();
+	fun6();
+	fun7();
+	fun8();
+	fun9();
+	fun10();
+	fun11();
+	fun12();
+	fun13();
+	fun14();
+	fun15();
+	fun16();
+	fun17();
+	fun18();
+	fun19();
+	fun20();
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun1() { console.log("this is a cool function called fun1!");}
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun2() { console.log("this is a cool function called fun2!");}
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun3() { console.log("this is a cool function called fun3!");}
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun4() { console.log("this is a cool function called fun4!");}
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun5() { console.log("this is a cool function called fun5!");}
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun6() { console.log("this is a cool function called fun6!");}
+
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun7() { console.log("this is a cool function called fun7!");}
+
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun8() { console.log("this is a cool function called fun8!");}
+
+
+/***/ },
+/* 9 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun9() { console.log("this is a cool function called fun9!");}
+
+
+/***/ },
+/* 10 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun10() { console.log("this is a cool function called fun10!");}
+
+
+/***/ },
+/* 11 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun11() { console.log("this is a cool function called fun11!");}
+
+
+/***/ },
+/* 12 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun12() { console.log("this is a cool function called fun12!");}
+
+
+/***/ },
+/* 13 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun13() { console.log("this is a cool function called fun13!");}
+
+
+/***/ },
+/* 14 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun14() { console.log("this is a cool function called fun14!");}
+
+
+/***/ },
+/* 15 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun15() { console.log("this is a cool function called fun15!");}
+
+
+/***/ },
+/* 16 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun16() { console.log("this is a cool function called fun16!");}
+
+
+/***/ },
+/* 17 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun17() { console.log("this is a cool function called fun17!");}
+
+
+/***/ },
+/* 18 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun18() { console.log("this is a cool function called fun18!");}
+
+
+/***/ },
+/* 19 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun19() { console.log("this is a cool function called fun19!");}
+
+
+/***/ },
+/* 20 */
+/***/ function(module, exports) {
+
+	module.exports =  function fun20() { console.log("this is a cool function called fun20!");}
+
+
+/***/ }
+/******/ ]);
