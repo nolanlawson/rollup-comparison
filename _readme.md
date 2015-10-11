e9ed59deb345fd6034566fe9676d913ebde7c781
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-11 01:40:27 -0400

    initial

---
+++ _readme.md
@@ -0,0 +1,20 @@
+compare rollup to browserify/webpack
+----
+
+usage:
+
+```
+npm run browserify
+npm run webpack
+npm run rollup
+```
+
+result (bytes):
+
+```
+6274	dist/bundle-browserify.js
+1652	dist/bundle-rollup.js
+7442	dist/bundle-webpack.js
+```
+
+
---
+++ dist/bundle-browserify.js
@@ -0,0 +1,284 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun1 = fun1;
+
+function fun1() {
+  console.log("this is a cool function called fun1!");
+}
+},{}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun10 = fun10;
+
+function fun10() {
+  console.log("this is a cool function called fun10!");
+}
+},{}],3:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun11 = fun11;
+
+function fun11() {
+  console.log("this is a cool function called fun11!");
+}
+},{}],4:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun12 = fun12;
+
+function fun12() {
+  console.log("this is a cool function called fun12!");
+}
+},{}],5:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun13 = fun13;
+
+function fun13() {
+  console.log("this is a cool function called fun13!");
+}
+},{}],6:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun14 = fun14;
+
+function fun14() {
+  console.log("this is a cool function called fun14!");
+}
+},{}],7:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun15 = fun15;
+
+function fun15() {
+  console.log("this is a cool function called fun15!");
+}
+},{}],8:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun16 = fun16;
+
+function fun16() {
+  console.log("this is a cool function called fun16!");
+}
+},{}],9:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun17 = fun17;
+
+function fun17() {
+  console.log("this is a cool function called fun17!");
+}
+},{}],10:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun18 = fun18;
+
+function fun18() {
+  console.log("this is a cool function called fun18!");
+}
+},{}],11:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun19 = fun19;
+
+function fun19() {
+  console.log("this is a cool function called fun19!");
+}
+},{}],12:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun2 = fun2;
+
+function fun2() {
+  console.log("this is a cool function called fun2!");
+}
+},{}],13:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun20 = fun20;
+
+function fun20() {
+  console.log("this is a cool function called fun20!");
+}
+},{}],14:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun3 = fun3;
+
+function fun3() {
+  console.log("this is a cool function called fun3!");
+}
+},{}],15:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun4 = fun4;
+
+function fun4() {
+  console.log("this is a cool function called fun4!");
+}
+},{}],16:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun5 = fun5;
+
+function fun5() {
+  console.log("this is a cool function called fun5!");
+}
+},{}],17:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun6 = fun6;
+
+function fun6() {
+  console.log("this is a cool function called fun6!");
+}
+},{}],18:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun7 = fun7;
+
+function fun7() {
+  console.log("this is a cool function called fun7!");
+}
+},{}],19:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun8 = fun8;
+
+function fun8() {
+  console.log("this is a cool function called fun8!");
+}
+},{}],20:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fun9 = fun9;
+
+function fun9() {
+  console.log("this is a cool function called fun9!");
+}
+},{}],21:[function(require,module,exports){
+'use strict';
+
+var _fun1 = require('./fun1');
+
+var _fun2 = require('./fun2');
+
+var _fun3 = require('./fun3');
+
+var _fun4 = require('./fun4');
+
+var _fun5 = require('./fun5');
+
+var _fun6 = require('./fun6');
+
+var _fun7 = require('./fun7');
+
+var _fun8 = require('./fun8');
+
+var _fun9 = require('./fun9');
+
+var _fun10 = require('./fun10');
+
+var _fun11 = require('./fun11');
+
+var _fun12 = require('./fun12');
+
+var _fun13 = require('./fun13');
+
+var _fun14 = require('./fun14');
+
+var _fun15 = require('./fun15');
+
+var _fun16 = require('./fun16');
+
+var _fun17 = require('./fun17');
+
+var _fun18 = require('./fun18');
+
+var _fun19 = require('./fun19');
+
+var _fun20 = require('./fun20');
+
+(0, _fun1.fun1)();
+(0, _fun2.fun2)();
+(0, _fun3.fun3)();
+(0, _fun4.fun4)();
+(0, _fun5.fun5)();
+(0, _fun6.fun6)();
+(0, _fun7.fun7)();
+(0, _fun8.fun8)();
+(0, _fun9.fun9)();
+(0, _fun10.fun10)();
+(0, _fun11.fun11)();
+(0, _fun12.fun12)();
+(0, _fun13.fun13)();
+(0, _fun14.fun14)();
+(0, _fun15.fun15)();
+(0, _fun16.fun16)();
+(0, _fun17.fun17)();
+(0, _fun18.fun18)();
+(0, _fun19.fun19)();
+(0, _fun20.fun20)();
+},{"./fun1":1,"./fun10":2,"./fun11":3,"./fun12":4,"./fun13":5,"./fun14":6,"./fun15":7,"./fun16":8,"./fun17":9,"./fun18":10,"./fun19":11,"./fun2":12,"./fun20":13,"./fun3":14,"./fun4":15,"./fun5":16,"./fun6":17,"./fun7":18,"./fun8":19,"./fun9":20}]},{},[21]);
